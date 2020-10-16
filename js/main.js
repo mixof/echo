f6e5a3f591048d318e5130580b6d7c52e639b467
Aleksey Ilyuchenko <mixof777@gmail.com>
2020-10-16 11:55:26 +0300

    Homepage latest version + responsive

--- js/main.js
+++ js/main.js
@@ -12,7 +12,54 @@ $(document).ready(function(e){
     });
 
 
+    var swiper1 = new Swiper('.swiper-container.members-list', {
+        slidesPerView: 3,
+        spaceBetween: 15,
+        loop: true,
+        pagination: {
+            el: '.members-list .swiper-pagination',
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            0: {
+                slidesPerView: 1
+            },
 
+           520:{
+                slidesPerView: 2,
+            },
+
+           820: {
+                slidesPerView: 3,
+                spaceBetween: 15
+            }
+        }
+    });
+
+
+    var swiper2 = new Swiper('.tips-slider .swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 12,
+        loop: true,
+        pagination: {
+            el: '.tips-slider .swiper-pagination',
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            0: {
+                slidesPerView: 1
+            },
+
+            520:{
+                slidesPerView: 2,
+            },
+
+            820: {
+                slidesPerView: 3,
+                spaceBetween: 15
+            }
+        }
+    });
 
 
 });
